fd74ffc077e57b5d31d966ef8219f5842be75071
paulomtts <paulomtts@outlook.com>
2023-11-16 23:07:56 -0300

    navbar small changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 
 import NavbarItem from "./NavbarItem";
 import "./Navbar.css";
 
 
+
 export default function Navbar(props) {
-    return (<>
-        <div className="Navbar">
-            <NavbarItem text="Recipes" onClick={() => props.onClickItem('recipes')}/>
-            <NavbarItem text="Ledger" onClick={() => props.onClickItem('registry')}/>
-        </div>
-    </>);
+    const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
+    const [scrollDirection, setScrollDirection] = useState("down"); 
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            if (prevScrollPos > currentScrollPos) {
+                setScrollDirection("up");
+            } else if (prevScrollPos < currentScrollPos) {
+                setScrollDirection("down");
+            }
+            setPrevScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [prevScrollPos]);
+
+    return (
+        <>
+            <div className={`Navbar ${scrollDirection === "down" ? "slide-out" : "slide-in"}`}>
+                <NavbarItem
+                    text="Recipes"
+                    onClick={() => props.onClickItem("recipes")}
+                />
+                <NavbarItem
+                    text="Ledger"
+                    onClick={() => props.onClickItem("registry")}
+                />
+            </div>
+        </>
+    );
 }
