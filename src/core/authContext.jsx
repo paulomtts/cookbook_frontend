250066d2a8585f69acfdfcf0680061f643df0341
paulomtts <paulomtts@outlook.com>
2023-12-10 13:14:58 -0300

    heroku deploy

--- src/core/authContext.jsx
+++ src/core/authContext.jsx
@@ -16,9 +16,11 @@ export function AuthProvider({ children }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     useEffect(() => {
-        // const jwtToken = document.cookie.split('; ').find(row => row.startsWith('cbk_s=')).split('=')[1];
-        // console.log(jwtToken);
-        // console.log('here')
+        if(document.cookie) {
+            const jwtToken = document.cookie.split('; ').find(row => row.startsWith('cbk_s=')).split('=')[1];
+            console.log(jwtToken);
+            console.log('here');
+        }
 
         const validateSession = async () => {
             const payload = {
