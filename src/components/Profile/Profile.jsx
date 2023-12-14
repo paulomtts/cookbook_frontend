a9a595e2eb8ef3a9d24bd07549a8735e4c66303a
paulomtts <paulomtts@outlook.com>
2023-12-14 16:20:15 -0300

    improved profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,47 @@
 import React from "react";
 
+import { useConfigs } from "../../core/configsContext";
+import { Form, Image, Badge } from "react-bootstrap";
+
+import './Profile.css';
+
 
 export default function Profile() {
+    const { user } = useConfigs();
+
     return (<>
-        <div className="centerize fade-in-long">
+        <div className="centerize fade-in-long Container">
 
-            <p className="f-size-4">
-                Page under construction
-            </p>
+            <div className="flex vertical gap-2 Card">
 
+                <div className="User flex align-center justify-center gap-4">
+                    <span className="name">{user.name}</span>
+                    <Image
+                        src={user.picture}
+                        roundedCircle
+                        className="ml-2"
+                        style={{width: '80px', height: '80px'}}
+                    />
+
+                </div>
+
+                <div className="flex justify-start">
+                    <span>
+                        1 recipe(s)
+                    </span>
+                </div>
+
+                <div>
+                    <div className="Badges-Container flex gap-2">
+                        <Badge className="Badge" bg="dark">
+                            pasta
+                        </Badge>
+                        <Badge className="Badge" bg="dark">
+                            meat
+                        </Badge>
+                    </div>
+                </div>
+            </div>
         </div>
     </>);
 }
